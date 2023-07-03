c7047d747b705ce582a3ee843a874f4282968939
Miska Alexia <miska.alexia@proton.me>
2023-07-02 22:37:29 -0700

    implement all the hard bits

--- src/main/java/io/github/VoidAndCaffeine/voids_mod_loader/FileUtilities.java
+++ src/main/java/io/github/VoidAndCaffeine/voids_mod_loader/FileUtilities.java
@@ -9,58 +9,65 @@ import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 /**
  * FileUtilities
  */
 public class FileUtilities {
     public static final org.slf4j.Logger VMLlog = LoggerFactory.getLogger("Void's Mod Loader");
     private static File VMlStaging = new File("VMLStaging");
+    private static File modsFolder = new File("mods");
 
-    public static boolean downloadFile(URL url,String fileName){
+    public static boolean downloadMod(URL url,String fileName){
             File file = new File(VMlStaging,fileName);
         try {
-
-            if(!VMlStaging.exists()){
-                VMlStaging.mkdirs();
-            }
             if(!file.exists()){
                 file.createNewFile();
             }
 
-            InputStream in = url.openStream();
-            ReadableByteChannel readableByteChannel = Channels.newChannel(in);
-            FileOutputStream fi = new FileOutputStream(file);
-            fi.getChannel().transferFrom(readableByteChannel,0,1 << 24);
+            downloadFile(url, file);
 
             return true;
-
         } catch (MalformedURLException e) {
             VMLlog.error("no network connection (or void messed up the url, but probably the internet thing -- definately -- 100%)");;
         } catch (IOException f){
-            VMLlog.error("io exception in downloadFile, you souldent see this. if you do, panic, then @me -- void");;
+            VMLlog.error("io exception in downloadFile, you souldent see this. if you do, panic, then @me -void");;
         }
         return false;
-
+    }
+    public static void downloadFile(URL url,File file) throws FileNotFoundException, IOException{
+ 
+        InputStream in = url.openStream();
+        ReadableByteChannel readableByteChannel = Channels.newChannel(in);
+        FileOutputStream fi = new FileOutputStream(file);
+        fi.getChannel().transferFrom(readableByteChannel,0,1 << 24);
+       
     }
 
-    public static String[][] processVFile(int mode){
+    public static void obOut(Object o, String filename) throws FileNotFoundException, IOException{
+        FileOutputStream fs = new FileOutputStream(filename);
+        ObjectOutputStream out = new ObjectOutputStream(fs);
+        out.writeObject(o);
+        out.close();
+    }
+
+    public static Object obIN(File file) throws FileNotFoundException, IOException{
+        FileInputStream fin = new FileInputStream(file);
+        ObjectInputStream oin = new ObjectInputStream(fin);
+        return oin;
+    }
+
+    public static String[][] processVFile(){
         try {
             URL vFileURL = new URL("https://raw.githubusercontent.com/VoidAndCaffeine/mods-versions-file/main/mods.versions");
 
             //VMLlog.info("file found");;
-            InputStream workingStream = null;
-            if(mode == 0){
-                workingStream = vFileURL.openStream();
-            }else if(mode == 1){
-                File localVfile = new File(VMlStaging,"mods.versions");
-                FileInputStream fin = new FileInputStream(localVfile);
-                workingStream = fin;
-            }
-            ObjectInputStream in = new ObjectInputStream(workingStream);
+            ObjectInputStream in = new ObjectInputStream(vFileURL.openStream());
             VMLlog.info("[VML] Created Object input stream");;
             
             String[][] working = (String[][]) in.readObject();
--- src/main/java/io/github/VoidAndCaffeine/voids_mod_loader/VoidsModLoader.java
+++ src/main/java/io/github/VoidAndCaffeine/voids_mod_loader/VoidsModLoader.java
@@ -1,5 +1,6 @@
 package io.github.VoidAndCaffeine.voids_mod_loader;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -15,25 +16,81 @@ public class VoidsModLoader implements ModInitializer {
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger VMLlog = LoggerFactory.getLogger("Void's Mod Loader");
-	private String[][] modsVNew;
-	private String[][] modsVOld;
+    private static File VMlStaging = new File("VMLStaging");
+	private String[][] mods;
+	private File vFileV = new File("mods.versions");
+	private boolean doUpdate;
+	private boolean forceUpdate = false;
 
 	@Override
 	public void onInitialize(ModContainer mod) {
-		modsVNew = FileUtilities.processVFile(0);
-		modsVOld = FileUtilities.processVFile(1);
+		try {
 
+            if(!VMlStaging.exists()){
+                VMlStaging.mkdirs();
+            }
+			
+		mods = FileUtilities.processVFile();
+		VMLlog.info("[VML] Imported vfile from github");
+
+		if(vFileV.exists()){
+			String verF = (String) FileUtilities.obIN(vFileV);
+			if(mods[0][2].equals(verF)){
+				doUpdate = false;
+				VMLlog.info("[VML] no updates found.");
+			} else{
+				doUpdate = true;
+
+				VMLlog.info("[VML] updates found.");
+			}
+		} else{
+			downloadUpdates(mods);
+			doUpdate = forceUpdate = true;
+
+			VMLlog.info("[VML] Done performing first install.");
+		}
+
+		if(new File("FORCEUPDATE.txt").exists()){
+			downloadUpdates(mods);
+			doUpdate = forceUpdate = true;
+			VMLlog.info("[VML] FORCEUPDATE.txt found, forcing a full update of all mods");
+		}
+
+		if(doUpdate && !(forceUpdate)){
+			downloadUpdates(findUpdates());
+		}
+
+		FileUtilities.downloadFile(, vFileV);
+
+		} catch (Exception e) {
+
+				VMLlog.error("[VML] Something went wrong and idk what :D -void");
+			// TODO: handle exception
+		}
 		//VMLlog.info("Hello Quilt world from {}!", modsV[0][0]);
+		/*
 		if (!(modsVNew[0][0].equals(""))) {
 			VMLlog.info("[VML] Successfully updated modlist.");
 		} else {
 			VMLlog.error("[VML] Modlist was not successfully updated.");
 			
-		}
+		}*/
 		
 	}
 
 	private String[][] findUpdates(){
+		String[][] toReturn = new String[Integer.parseInt(mods[0][0])][4];
+		int nextEmpty = 0;
+		for (int i = 1; i < mods.length; i++) {
+			if(Integer.parseInt(mods[i][3]) == 1){
+				toReturn[nextEmpty] = mods[i];
+				nextEmpty++;
+			}
+		}
+
+		return toReturn;
+
+		/*
 		String[][] buffer = new String[modsVNew.length][4];
 		int nextEmpty = 0;
 		if(modsVNew.length>modsVOld.length){
@@ -55,6 +112,7 @@ public class VoidsModLoader implements ModInitializer {
 			toReutrn[i]= buffer[i];
 		}
 		return toReutrn;
+		*/
 	}
 
 	private void downloadUpdates(String[][] updates){
@@ -62,18 +120,13 @@ public class VoidsModLoader implements ModInitializer {
 		for (int i = 0; i < updates.length; i++) {
 			try {
 				URL modurl = new URL(updates[i][3]);
-				FileUtilities.downloadFile(modurl, updates[i][1]);
-				VMLlog.info("[VML] Successfuly downloaded mod updates.");
+				if(FileUtilities.downloadMod(modurl, updates[i][1])){
+
+				}
+				VMLlog.info("[VML] Successfuly downloaded mod | {} | update", updates[i][0]);
 			} catch (MalformedURLException e) {
 				VMLlog.error("[VML] Check your wifi -void | error on mod {}!", updates[i][0]);
 			}
 		}
-		try {
-            URL vFileURL = new URL("https://raw.githubusercontent.com/VoidAndCaffeine/mods-versions-file/main/mods.versions");
-			FileUtilities.downloadFile(vFileURL,"mods.versions");
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-	
 	}
 }
